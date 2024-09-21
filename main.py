ea2e202fb14b927093759534ce84eb6ee1c85bdf
Vincent <91401445+TCVinLee@users.noreply.github.com>
2024-09-21 19:53:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from source_code import fuel_cost_calculator as fc
+
+fc.intro()
+fc.result()
---
+++ source_code/fuel_cost_calculator.py
@@ -0,0 +1,67 @@
+#1 imperial gallon = 4.54609188 litres
+#per litre: 46/ 4.54609188 = 10.1185812 miles
+#1 mile = 0.0988376 litre
+
+#Set up function to check if input is float
+def check_input(prompt):
+  while True:
+    user_input = input(prompt)
+    try:
+      user_input = float(user_input)
+      return user_input
+    except ValueError:
+      print(f"It requires a number and you have entered '{user_input}'. Please enter a valid number. For example 46 or 37.85 \n\n")
+    
+
+def intro():
+  #Print out title
+  print("\n\n")
+  print("*********************************")
+  print("*     Fuel Cost Calculator      *")
+  print("*     MPG to KMpL convertor     *")
+  print("*                           v3.0*")
+  print("*********************************")
+  print("\n\n")
+
+  #Instruction for using calculator or convertor functionality
+  print("* If you only want to convert MPG to Km/L, please enter 0 for the following question *\n")
+  return
+
+
+def questions():
+  #Enter MPG information
+  #mpg = input("Enter the number for MPG: ")
+  mpg = check_input("Enter the number for MPG: ")
+  #Enter fuel cost (in pence)
+  cost = check_input("Enter the cost of fuel (in pence): ")
+  #Enter distance in miles
+  distance = check_input("Enter the distance in miles :")
+  return mpg, cost, distance
+
+
+def fuel_cal():
+  #Convert input to float
+  #mpg = float(mpg)
+  mpg, cost, distance = questions()
+  #Break down for miles per litre
+  miles_per_litre = mpg / 4.54609188
+  #Calculate how many KM per litre
+  per_litre_for_km = "{:.2f}".format(miles_per_litre * 1.60934)
+  #Calculate distance in km
+  distance_in_km = "{:.2f}".format(distance * 1.60934)
+  #Calculate how much litre needed for 1 mile
+  litres_per_mile = 1 / miles_per_litre
+  #Calculate how much litres needed for the distance
+  litres_needed = litres_per_mile * distance
+  #Calculate cost for the distance
+  cost_needed = litres_needed * cost
+  #cost in GBP, round to 2 decimal points
+  cost_in_GBP = "{:.2f}".format(cost_needed / 100)
+  return cost_in_GBP, distance, distance_in_km, mpg, per_litre_for_km
+
+def result():
+  cost_in_GBP, distance, distance_in_km, mpg, per_litre_for_km = fuel_cal()
+
+  print(f"\n\n*****Cost for the trip*****\n\nCost: £{cost_in_GBP}\n\nDistance: {distance} miles | {distance_in_km} km.\n")
+  print(f"* Fuel efficiency * \nMPG: {mpg} = km/l: {per_litre_for_km}.\n\n")
+  return
